e538ed155a294458184c0cb57741368407db8f78
SteveQiu <qiuyuliang20042005@hotmail.com>
2015-07-19 14:28:50 -0700

    port

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('navigation http://%s:%s', host, port);
 });
