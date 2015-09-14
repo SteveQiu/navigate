8755295b1312cb16685d0c9e1f8d868e035711d7
SteveQiu <qiuyuliang20042005@hotmail.com>
2015-09-14 00:14:37 -0700

    add footer

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,6 +1,9 @@
 var app = angular.module("menu", []);
 app.controller("myCtrl", function ($scope) {
   $scope.winHeight = window.innerHeight;
+  $scope.cardHeight = Math.min(Math.floor(($scope.winHeight - 70) * 7 / 8), 800);
+  $scope.footerHeight = $scope.winHeight - 50 - $scope.cardHeight;
+  console.log($scope.footerHeight);
   $scope.tag = 'home';
   $scope.cards = [{
     'img': './img/php.jpg',
@@ -10,8 +13,8 @@ app.controller("myCtrl", function ($scope) {
   }, {
     'img': './img/node.jpg',
     'env': 'NodeJs',
-    'link': '',
-    'content': 'This is the only node application currently'
+    'link': 'https://thechecklist.herokuapp.com/',
+    'content': 'This is only a testing prototype of an application'
   }, {
     'img': './img/django.jpg',
     'env': 'Django',
@@ -28,15 +31,20 @@ app.controller("myCtrl", function ($scope) {
     'link': '',
     'content': ''
   }];
-
-  $scope.setTag = function(tagName){
+  $scope.message = {
+    'about': 'This is a brief introduction',
+    'contact': 'steveqiu@hotmail.ca'
+  };
+  $scope.setTag = function (tagName) {
     $scope.tag = tagName;
   }
-
-  $scope.pickClass = function(tagName){
+  $scope.pickClass = function (tagName) {
     if ($scope.tag == tagName) {
       return 'active';
     };
     return '';
+  }
+  $scope.checkTag = function (tagName) {
+    return $scope.tag === tagName;
   }
 });
