00645bebf459b328f9d21b66db030872a7bf6235
SteveQiu <qiuyuliang20042005@hotmail.com>
2020-08-08 10:36:27 -0700

    update

--- server.js
+++ server.js
@@ -1,56 +1,14 @@
 #!/usr/bin/env node
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var errorRouter = require('./app/error');
 var http = require('http');
+var app = require('./app/app');
 var port = process.env.PORT || '3000';
-
-var app = express();
-// view engine setup for render function
-app.set('port', port);
-app.set('views', path.join(__dirname, 'render'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/error', errorRouter);// can remove
-app.use('/', (req,res)=>{
-  res.redirect('/index.html');
-});
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 /**
  * Create HTTP server.
  */
-/**
- * Listen on provided port, on all network interfaces.
- */
-
 var server = http.createServer(app);
 server.listen(port);
 console.log('Server running at port ', port);
-
 /**
  * Event listener for HTTP server "error" event.
  */
@@ -58,7 +16,6 @@ server.on('error', (error)=>{
   if (error.syscall !== 'listen') {
     throw error;
   }
-
   var bind = 'Port ' + port;
 
   // handle specific listen errors with friendly messages
