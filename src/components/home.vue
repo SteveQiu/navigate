f01ddb4669ba5d9fa73b3e2b200bd0be6dc8b783
SteveQiu <qiuyuliang20042005@hotmail.com>
2020-08-09 12:51:42 -0700

    refactor

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,19 +1,22 @@
 <template>
 <div class="container">
-  <div class="img img-city rellax" data-rellax-speed="6"></div>
-  <div class="img img-platform rellax" data-rellax-speed="1"></div>
-  <div class="img img-title rellax" data-rellax-speed="-3">WELCOME!</div>
-  <div class="parallax-fillter"></div>
+  <welcome-banner/>
   <h1>Steve's Web Directory</h1>
   <h3>This is a VueJS Webpack NodeJS Project. It is used as a directory for Steve's website.</h3>
-  <div class="parallax-fillter"></div>
+  <encouragement-banner/>
 </div>
 </template>
 <script>
 let Rellax = require('rellax');
+import WelcomeBanner from './Welcome'
+import EncouragementBanner from './Encouragement'
 
 export default {
   name:'home',
+  components:{
+    WelcomeBanner,
+    EncouragementBanner,
+  },
   mounted: function () {
     var rellax = new Rellax('.rellax', {
       center: false,
@@ -48,38 +51,4 @@ h3{
   color: #4c4c4c;
   text-align: center;
 }
-
-.img{
-  width: 100%;
-  min-height: 100vh;
-  background-color: transparent;
-  background-repeat: no-repeat;
-  background-size: cover;
-  position:absolute;
-}
-
-.img-city{
-  background-image: url('/img/night-city.png');
-  top: 0%;
-  min-height: 110vh;
-}
-.img-title{
-  text-align: center;
-  font-size: 4em;
-  top: 50%;
-  color: white;
-  font-weight: bold;
-}
-.img-platform{
-  background-image: url('/img/platform.png');
-  position:absolute;
-  top: 0%;
-}
-
-.parallax-fillter{
-  width: 100%;
-  min-height: 100vh;
-}
-
-
 </style>
